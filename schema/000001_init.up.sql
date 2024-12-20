b498513a4552dc9b8d27c5cf0cef883df6e4dfbe
speeddem0n <slavik2890@gmail.com>
2024-12-20 15:50:42 +0300

    Migrations init v2

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -6,26 +6,26 @@ CREATE TABLE users (
 );
 
 CREATE TABLE todo_lists (
-    id SERIAL PRIMARY KEY,,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255)
 );
 
 CREATE TABLE users_list (
-    id SERIAL PRIMARY KEY,,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (list_id) REFERENCES todo_lists(id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    user_id int REFERENCES users (id) ON DELETE CASCADE NOT NULL,
+    list_id int REFERENCES todo_lists (id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE todo_items (
-    id SERIAL PRIMARY KEY,,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255),
     done BOOLEAN NOT NULL DEFAULT false 
 );
 
 CREATE TABLE lists_items (
-    id SERIAL PRIMARY KEY,,
-    FOREIGN KEY (item_id) REFERENCES todo_items (id) on DELETE CASCADE,
-    FOREIGN KEY (list_id) REFERENCES todo_lists (id) on DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    item_id int REFERENCES todo_items (id) on DELETE CASCADE NOT NULL,
+    list_id int REFERENCES todo_lists (id) on DELETE CASCADE NOT NULL
 );
