9103b4f8c2e6a3f2605c1a0a1600076c233cda47
speeddem0n <slavik2890@gmail.com>
2025-01-15 18:18:16 +0300

    added auto-migration on application start

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,10 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/joho/godotenv"   // godotenv для работы с .env файлами
-	_ "github.com/lib/pq"        // драйвер для работы с БД
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv" // godotenv для работы с .env файлами
+	_ "github.com/lib/pq"      // драйвер для работы с БД
+	"github.com/pressly/goose"
 	"github.com/sirupsen/logrus" // logrus для логирования
 	"github.com/speeddem0n/todoapp/internal/handler"
 	"github.com/speeddem0n/todoapp/internal/repository"
@@ -16,6 +18,16 @@ import (
 	"github.com/speeddem0n/todoapp/internal/service"
 	"github.com/spf13/viper" // viper для работы с config файлами
 )
+
+func runMigrations(db *sqlx.DB) {
+	migrationsDir := "./internal/repository/migrations" // Путь к папке с миграциями
+
+	logrus.Info("Running database migrations...")
+	if err := goose.Up(db.DB, migrationsDir); err != nil {
+		logrus.Fatalf("Failed to apply migrations: %v", err)
+	}
+	logrus.Info("Database migrations applied successfully.")
+}
 
 // @title TodoAppApi
 // @version 1.0
@@ -51,6 +63,9 @@ func main() {
 	if err != nil {
 		logrus.Fatalf("failed to initialize db: %s", err.Error())
 	}
+
+	// Запускаем миграции при старте приложения
+	runMigrations(db)
 
 	repos := repository.NewRepository(db)    // Ицициализируем структуру БД (4 УРОВЕНЬ)
 	services := service.NewService(repos)    // Инициализируем структуру сервисов и передаем в нее структуру БД (3 УРОВЕНЬ)
